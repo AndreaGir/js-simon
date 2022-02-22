e387a17ae2cf506871899ee4cc21acd7ce83daa8
AndreaGir <girolamiandrea2000@gmail.com>
2022-02-22 16:16:43 +0100

    1

---
+++ app.js
@@ -0,0 +1,15 @@
+const numeriGioco = ['10','22','74','3','56']
+
+for(let i = 0; i < numeriGioco.length; i++){
+
+    const visualizzareNumeri = alert(`Ricorda questi numeri    " ${numeriGioco[i]} "`)
+
+
+}
+
+
+const tempo = 10; 
+
+ const divTempo = document.querySelector('.tempo')
+
+ divTempo.innerHTML = tempo
