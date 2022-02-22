14a483d38f1165cc71fb2428eb7e9a242f50d3ad
AndreaGir <girolamiandrea2000@gmail.com>
2022-02-22 19:11:24 +0100

    ,,

--- app.js
+++ app.js
@@ -1,38 +1,79 @@
-const numeriGioco = ['10','22','74','3','56']
+let numeriCasuali;
+
+let numeriComputer;
+
+/* creo numeri casuali  */
+
+function numeriRandom(min , max){
+    return Math.floor(Math.random() * (max - min + 1) + min)
+    }
+
+
+
+let  numeriGioco = []
+
+    for(let i = 0; i < 5; i++ ){
+        let numeri = numeriRandom(1 , 100)
+        numeriGioco.push(numeri)
+        
+    }
+    
 
 for(let i = 0; i < numeriGioco.length; i++){
 
     const visualizzareNumeri = alert(`Ricorda questi numeri    " ${numeriGioco[i]} "`)
 
-
+    
 }
 
 
-let tempo = 5; 
+let tempo = 2; 
 
  let divTempo = document.querySelector('.tempo')
 
  divTempo.innerHTML = `mancano ${tempo} secondi `
 
- 
+ let numeriScritti = []
 
- const cronometro = setInterval( funzioneCronometro , 1000 )
+ let numeriUtente;
+
+ const cronometro = setInterval( funzioneCronometro , 950 )
 
  function funzioneCronometro(){
     tempo--
     divTempo.innerHTML = `mancano ${tempo} secondi `
 
+    
+
     if(tempo === 0){
         clearInterval( cronometro )
         divTempo.innerHTML = 'Tempo terminato!'
+        
 
+        for(let i = 0; i < numeriGioco.length; i++){
+            numeriUtente =  prompt('Ora scrvimi i numeri che ti ho fatto vedere prima')
+        }   
+        
+        
+
+        
     }
  }
+ray
 
- for(let i = 0; i > 5; i++){
-     
- }
+    
 
+
+
+
+
+
+       
+ 
+
+  
+
+ 
  
      
     
